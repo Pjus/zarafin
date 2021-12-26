8f83e896a77807275c5665130ca17deb39ab9369
Pjus <wnstjd117@naver.com>
2021-12-26 23:45:30 +0900

    add fcf db

--- search/views.py
+++ search/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from . import forms
 from .models import *
-
+import sqlite3
 
 # Create your views here.
 def search(request):
-    QGD = QuantGaloreData(Ticker='AAPL')
+    ticker = request.GET.get('ticker').upper()
+    QGD = QuantGaloreData(Ticker=ticker)
     data, z_value = QGD.find_z()
+    con = sqlite3.connect("./stocks_DB.db")
+    df = pd.read_sql("SELECT * FROM cap_company", con)
+    con.close()
     context = {
-        'ticker' : request.GET.get('ticker').upper(),
+        'ticker' : ticker,
         'data' : data.to_html(),
         'z_value' : round(z_value.values[0], 2),
+        'market_cap': df.to_html(),
     }
     return render(request, "search/search_main.html", context=context)
