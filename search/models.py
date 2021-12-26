7610e39ab1ac2afd36376f9b13336336417aa5ee
Pjus <wnstjd117@naver.com>
2021-12-26 20:32:21 +0900

    add app

--- search/models.py
+++ search/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import yfinance as yf
+import pandas as pd
+import numpy as np
 
 # Create your models here.
+class QuantGaloreData:
+    def __init__(self, Ticker):
+        self.ticker = yf.Tickers(Ticker)
+        self.data = yf.download(tickers=(Ticker),period='1y',interval='1d',group_by='ticker',auto_adjust=True,prepost=False)
+        self.df = pd.DataFrame(self.data)
+        # print(self.df.tail(1))
+    def find_z(self):
+        mean = self.df['Close'].mean()
+        z_from_mean = (self.df['Close'].tail(1) - mean) / np.std(self.df['Close'])
+        # print(self.ticker,self.df['Close'].tail(1),z_from_mean)
+
+        return self.df, z_from_mean
--- search/views.py
+++ search/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from . import forms
+from .models import *
 
 
 # Create your views here.
 def search(request):
-    print(request.GET.get('ticker'))
+    QGD = QuantGaloreData(Ticker='AAPL')
+    data, z_value = QGD.find_z()
     context = {
-        'ticker' : request.GET.get('ticker').upper()
+        'ticker' : request.GET.get('ticker').upper(),
+        'data' : data.to_html(),
+        'z_value' : round(z_value.values[0], 2),
     }
     return render(request, "search/search_main.html", context=context)
