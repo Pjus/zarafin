2c00ff251eb4a543cb316a5e089ef11c90a06b1e
Pjus <wnstjd117@naver.com>
2021-12-28 21:17:35 +0900

    add ratio table

--- search/views.py
+++ search/views.py
@@ -4,22 +4,14 @@ from .models import *
 import sqlite3
 import json
 
-# bokeh
-from bokeh.embed import components
-
-import bokeh
-import holoviews as hv
-from bokeh.sampledata.stocks import AAPL
-from holoviews.plotting.links import RangeToolLink
-from holoviews import opts
-
 # Create your views here.
 def search(request):
     ticker = request.GET.get('ticker').upper()
     QGD = QuantGaloreData(Ticker=ticker)
     prices, z_value = QGD.find_z()
     prices['Date'] = prices.index
-    # print(prices)
+    prices['Date'] = prices['Date'].astype(str)
+
     con = sqlite3.connect("./stocks_DB.db")
     market_cap = pd.read_sql("SELECT * FROM cap_company", con)
 
@@ -31,36 +23,23 @@ def search(request):
 
     company_name = market_cap[market_cap['Ticker']==ticker]['Name']
     con.close()
-
-    hv.extension('bokeh')
-    renderer = hv.renderer('bokeh')
-
-    # Make dataframe from stock data
-    aapl_df = pd.DataFrame(prices['Close'], columns=['Close'], index=pd.to_datetime(prices['Date']))
-    aapl_df.index.name = 'Date'
-
-    # Create stock curve
-    aapl_curve = hv.Curve(aapl_df, 'Date', ('Close', 'Price ($)'))
-    # Labels and layout
-    tgt = aapl_curve.relabel(f'{ticker} close price').opts(width=800, labelled=['y'], toolbar='disable')
-    src = aapl_curve.opts(width=800, height=100, yaxis=None, default_tools=[])
-
-    RangeToolLink(src, tgt)
-    # Merge rangetool
-    layout = (tgt + src).cols(1)
-    layout.opts(opts.Layout(shared_axes=False, merge_tools=False))
-
-    html = renderer.html(layout)
-    print(company_name.values)
+    closes = [round(x, 2) for x in prices['Close'].to_list()]
+    date = prices['Date'].to_list()
+    date = [x.replace("&quot;", "") for x in date]
+    temp = ratios[0:1]
+    temp_dict = {}
+    for i in temp:
+        temp_dict[i] = temp[i][0]
 
     context = {
         'name': company_name.values[0],
         'ticker' : ticker,
+        'date' : json.dumps(date),
         'prices' : prices.to_html(),
+        'close' : json.dumps(closes),
         'z_value' : round(z_value.values[0], 2),
         'market_cap': market_cap.to_html(),
         'free_cash_flow' : fcf.T.to_html(),
-        'key_ratios': ratios.T.to_html(),
-        'html':html,
+        'key_ratios': temp_dict,
     }
     return render(request, "search/search_main.html", context=context)
